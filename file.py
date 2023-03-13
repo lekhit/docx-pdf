58d6e28ba3f3f6b537b5701c6cc6be8769f3ab93
lekhit borole <blekhit@gmail.com>
2023-03-13 18:48:50 +0530

    added merge function

--- file.py
+++ file.py
@@ -1,7 +1,9 @@
 from docx2pdf import convert
 from docx import Document
-def replace_string(filename,old_text,new_text):
-    doc = Document(filename)
+from PyPDF2 import PdfMerger, PdfReader
+
+def replace_string(input_filename,output_filename,old_text,new_text):
+    doc = Document(input_filename)
     for p in doc.paragraphs:
         if old_text in p.text:
             inline = p.runs
@@ -12,11 +14,19 @@ def replace_string(filename,old_text,new_text):
                     inline[i].text = text
             print (p.text)
 
-    doc.save('data/dest1.docx')
+    doc.save(f'data/{output_filename}.docx')
     return 1
+
+
+def merge_pdfs(files_input:list,output_file:str):
+  merger = PdfMerger()
+  for file in files_input:
+    merger.append(PdfReader(open(file, 'rb')))
+  #merger.append(PdfFileReader(open(filename2, 'rb')))
+
+  merger.write(output_file)
 #replace_string('/Users/manas/Documents/dev_new/python/pdf-word/Beginners advice.docx','Me','Ghandi')
 
+#convert("data/dest1.docx", "data/output.pdf")
 
-
-
-convert("data/dest1.docx", "data/output.pdf")
+merge_pdfs([f'data/{i}' for i in ['1.pdf','2.pdf','3.pdf']],'data/merged.pdf')
